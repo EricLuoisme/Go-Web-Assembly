75af7db49693136e25f057b03e5ef86a37647931
roylic <lykismekk@outlook.com>
2023-05-01 17:19:05 +0800

    correct the loading

--- webassembly/cmd/button/main.go
+++ webassembly/cmd/button/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"syscall/js"
+)
 
 func main() {
 	fmt.Println("Button Clicked")
+
+	c := make(chan struct{}, 0)
+	registerCallbacks()
+	<-c
+}
+
+func add(_ js.Value, i []js.Value) interface{} {
+	result := js.ValueOf(i[0].Int() + i[1].Int())
+	js.Global().Set("output", result)
+	fmt.Println(result)
+	return result
+}
+
+func subtract(_ js.Value, i []js.Value) interface{} {
+	result := js.ValueOf(i[0].Int() - i[1].Int())
+	js.Global().Set("output", result)
+	fmt.Println(result)
+	return result
+}
+
+func registerCallbacks() {
+	js.Global().Set("add", js.FuncOf(add))
+	js.Global().Set("subtract", js.FuncOf(subtract))
 }
